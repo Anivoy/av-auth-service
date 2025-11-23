fd89124216c68723a0bd0ddd6ce38a7133db8a61
Fadil Hisyam <ichorvfx@gmail.com>
2025-11-23 14:13:59 +0700

    refactor: better cookie helper

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,27 +1,37 @@
 import { jwtConfig } from "../config/env.js";
 import durationToSeconds from "./durationToSeconds.js";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export function setAccessCookie(res, token) {
-  res.cookie('access_token', token, {
+  res.cookie("access_token", token, {
     httpOnly: true,
-    secure: process.env.COOKIE_SECURE === 'true',
-    domain: process.env.COOKIE_DOMAIN || undefined,
-    sameSite: process.env.COOKIE_SAME_SITE || 'lax',
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
+    domain: isProd ? process.env.COOKIE_DOMAIN : "localhost",
+    path: "/",
     maxAge: durationToSeconds(jwtConfig.JWT_EXPIRES_IN) * 1000
   });
 }
 
 export function setRefreshCookie(res, token) {
-  res.cookie('refresh_token', token, {
+  res.cookie("refresh_token", token, {
     httpOnly: true,
-    secure: process.env.COOKIE_SECURE === 'true',
-    domain: process.env.COOKIE_DOMAIN || undefined,
-    sameSite: process.env.COOKIE_SAME_SITE || 'lax',
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
+    domain: isProd ? process.env.COOKIE_DOMAIN : "localhost",
+    path: "/",
     maxAge: durationToSeconds(jwtConfig.JWT_REFRESH_EXPIRES_IN) * 1000
   });
 }
 
 export function clearAuthCookies(res) {
-  res.clearCookie('access_token');
-  res.clearCookie('refresh_token');
+  res.clearCookie("access_token", {
+    domain: isProd ? process.env.COOKIE_DOMAIN : undefined,
+    path: "/",
+  });
+  res.clearCookie("refresh_token", {
+    domain: isProd ? process.env.COOKIE_DOMAIN : undefined,
+    path: "/",
+  });
 }
